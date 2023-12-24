edde95f6f9e086b689143df82f95dd6929d7c51d
yuanyanmin <1414011709@qq.com>
2023-12-25 00:02:51 +0800

    轮播图功能开发

---
+++ src/services/home.ts
@@ -0,0 +1,13 @@
+import type { BannerItem } from "@/types/home"
+import { http } from "@/utils/http"
+
+// 首页广告区域
+export const getHomeBannerAPI = (distributionSite = 1) => {
+  return http<BannerItem[]>({
+    method: 'GET',
+    url: '/home/banner',
+    data: {
+      distributionSite
+    }
+  })
+}
